f782552b0e87b8491b782d0ec546323bf5f23138
Suraj <surajybtg@gmail.com>
2023-01-09 12:04:41 +0530

    Link updated

--- marriage-app/src/config/api.jsx
+++ marriage-app/src/config/api.jsx
@@ -1,2 +1,5 @@
-export const url = "http://localhost:5000/registrationData";
-export const url2 = "http://localhost:5000/users";
+// export const url = "http://localhost:5000/registrationData";
+// export const url2 = "http://localhost:5000/users";
+
+export const url = "https://marriage-portaldata.vercel.app/registrationData";
+export const url2 = "https://marriage-portaldata.vercel.app/users";
